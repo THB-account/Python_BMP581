03998742759aad9355f24a57267529b553ae57db
THB-account <56601190+THB-account@users.noreply.github.com>
2024-04-11 20:22:08 +0200

    Update simpleTest.py

--- examples/simpleTest.py
+++ examples/simpleTest.py
@@ -1,1 +1,21 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 Jose D. Montoya
+#
+# SPDX-License-Identifier: MIT
+
+import time
+from machine import Pin, I2C
+from micropython_bmp581 import bmp581
+from SMBUS_Adaption import ByteSMBus
+
+i2c = ByteSMBus(1) # Correct I2C pins for RP2040
+bmp = bmp581.BMP581(i2c)
+
+# Oddly first pressure measure after power-on return wrong value.
+# so we "burn" one measure
+_ = bmp.pressure
+
+while True:
+    print(f"Pressure: {bmp.pressure:.2f}kPa")
+    print()
+    time.sleep(0.5)
 
