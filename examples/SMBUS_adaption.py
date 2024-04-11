0f4e6234d6be618c8dcd22dd4945ed56dcb33bd9
THB-account <56601190+THB-account@users.noreply.github.com>
2024-04-11 20:16:54 +0200

    Update SMBUS_adaption.py
    Adaption of SMBus class to return bytes class on read

--- examples/SMBUS_adaption.py
+++ examples/SMBUS_adaption.py
@@ -1,1 +1,5 @@
+import smbus2
 
+class ByteSMBus(smbus2.SMBus):
+  def read_i2c_block_data(self,i2c_adrr,register,length,force=None):
+    return bytes(smbus2.SMBus.read_i2c_block_data(i2c_addr,register,length,force))
