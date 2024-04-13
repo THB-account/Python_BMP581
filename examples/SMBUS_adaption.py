0c0e370389cd8a99fdad2f83d696b748960ee6af
THB-account <56601190+THB-account@users.noreply.github.com>
2024-04-13 21:50:30 +0200

    Update SMBUS_adaption.py

--- examples/SMBUS_adaption.py
+++ examples/SMBUS_adaption.py
@@ -2,4 +2,4 @@ import smbus2
 
 class ByteSMBus(smbus2.SMBus):
   def read_i2c_block_data(self,i2c_adrr,register,length,force=None):
-    return bytes(smbus2.SMBus.read_i2c_block_data(i2c_addr,register,length,force))
+    return bytes(super().read_i2c_block_data(i2c_addr,register,length,force))
