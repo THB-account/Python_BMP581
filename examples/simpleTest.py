fb9da75d1ca7740dab76941c9759363a8edec54f
THB-account <56601190+THB-account@users.noreply.github.com>
2024-04-14 11:19:58 +0200

    Update simpleTest.py

--- examples/simpleTest.py
+++ examples/simpleTest.py
@@ -7,16 +7,17 @@ import time
 from bmp581 import bmp581
 from smbus_adaption import ByteSMBus
 
-i2c = ByteSMBus(1) # Correct I2C pins for RP2040
-bmp = bmp581.BMP581(i2c)
 
-# Oddly first pressure measure after power-on return wrong value.
-# so we "burn" one measure
-_ = bmp.pressure
-
-while True:
-    print(f"Pressure: {bmp.pressure:.2f}kPa")
-    print(f"Temperature: {bmp.temperature:.2f}°C")
-    print()
-    time.sleep(0.5)
+with ByteSMBus(1) as i2c:
+    bmp = bmp581.BMP581(i2c)
+    
+    # Oddly first pressure measure after power-on return wrong value.
+    # so we "burn" one measure
+    _ = bmp.pressure
+    
+    while True:
+        print(f"Pressure: {bmp.pressure:.2f}kPa")
+        print(f"Temperature: {bmp.temperature:.2f}°C")
+        print()
+        time.sleep(0.5)
 
